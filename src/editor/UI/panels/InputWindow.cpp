15fa4af7cdb3fe1cc5625f509364f7a2f9dcb477
Maxene Legaspi <andrea_legaspi@dlsu.edu.ph>
2026-07-16 13:56:51 +0800

    fix

--- src/editor/UI/panels/InputWindow.cpp
+++ src/editor/UI/panels/InputWindow.cpp
@@ -31,6 +31,106 @@ void InputWindow::Draw() {
     ImGuiWindowFlags flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize;
     if (ImGui::BeginPopupModal(this->GetName().c_str(), &this->show, flags)) {
         //if(ImGui::)
+
+        ImGui::SeparatorText("Select Input Video");
+        ImGui::Text("File name: ");
+        if (!this->hasInputVideo) {
+            if (ImGui::Button("Open File Browser")) {
+                fileDialog.Open();
+            }
+        }
+        else {
+            ImGui::SameLine();
+            HighlightImGuiText(inputFile, UIColor::YELLOW);
+            ImGui::SameLine();
+            RightAlignElement("Change##1");
+            if (ImGui::Button("Change##1")) {
+                hasInputVideo = false;
+                fileDialog.ClearSelected();
+                fileDialog.Open();
+            }
+        }
+
+        fileDialog.Display();
+
+        if (fileDialog.HasSelected()) {
+            hasInputVideo = true;
+            std::filesystem::path path = fileDialog.GetSelected();
+            inputFile = path.generic_string();
+        }
+        ImGui::NewLine();
+
+
+        //NAME
+        ImGui::BeginDisabled(!this->hasInputVideo);
+        ImGui::SeparatorText("Enter the name of the output folder");
+        ImGui::Text("Name: ");
+
+        if (!hasName) {
+            bool confirm = ImGui::InputText("##FolderName", this->inputText, sizeof(this->inputText), ImGuiInputTextFlags_EnterReturnsTrue);
+            ImGui::SameLine();
+            confirm |= ImGui::Button("Enter"); //either Enter key or button click
+
+            String errorMsg;
+            bool validName = ValidateFolderName(this->inputText, errorMsg);
+            if (!validName && this->inputText[0] != '\0') {
+                HighlightImGuiText(errorMsg, UIColor::RED);
+            }
+
+            if (confirm && validName) {
+                this->folderName = String(this->inputText);
+                this->hasName = true;
+                this->hasCompleteInput = true;
+            }
+        }
+        else {
+            ImGui::SameLine();
+            HighlightImGuiText(this->folderName, UIColor::YELLOW);
+            ImGui::SameLine();
+            RightAlignElement("Change##2");
+            if (ImGui::Button("Change##2")) {
+                this->hasName = false;
+            }
+        }
+        ImGui::EndDisabled();
+        ImGui::NewLine();
+
+        //MINIMUM FRAMES
+        ImGui::BeginDisabled(!this->hasName);
+        static int indexFrames = 0;
+
+        ImGui::SeparatorText("Select the number of Minimum Frames");
+        ImGui::Combo("Minimum Frames", &indexFrames, this->items.data(), this->items.size());
+        ImGui::EndDisabled();
+        ImGui::NewLine();
+
+        //QUALITY
+        ImGui::BeginDisabled(!this->hasName);
+        ImGui::SeparatorText("Select the quality of the model to be exported");
+        const char* options[] = { "fast", "medium", "detailed" };
+
+        static int indexQuality = 0;
+        ImGui::Combo("Export Quality", &indexQuality, options, IM_ARRAYSIZE(options));
+
+        ImGui::EndDisabled();
+        ImGui::NewLine();
+
+
+        ImGui::BeginDisabled(!this->hasCompleteInput);
+        if (ImGui::Button("Done")) {
+            OverviewPanel* overview = static_cast<OverviewPanel*>(UIManager::GetInstance()->GetPanelByType(UIType::OVERVIEW));
+            if (overview) {
+                overview->SetInput(
+                    this->inputFile,
+                    this->folderName,
+                    std::stoi(this->items[indexFrames]),
+                    options[indexQuality]);
+            }
+            this->activeSelf = false;
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::EndDisabled();
+        ImGui::EndPopup();
     }
 };
     
